5bce6663df181256eeef96df6398a341b577d25a
Fernando Tadashi Akimoto <fernando.akimoto@liferay.com>
2016-08-22 00:09:06 -0300

    Adicionado url para ingredientes.

--- src/main/java/hello/controller/GreetingController.java
+++ src/main/java/hello/controller/GreetingController.java
@@ -1,8 +1,11 @@
 package hello.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import hello.entity.Greeting;
+import hello.entity.Ingredients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,4 +21,13 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+
+    @RequestMapping("/ingredients")
+    public List<Ingredients> ingredients() {
+        List<Ingredients> ingredients = new ArrayList<>();
+        ingredients.add(new Ingredients("salt"));
+        ingredients.add(new Ingredients("sugar"));
+        return ingredients;
+    }
+
 }
---
+++ src/main/java/hello/entity/Ingredients.java
@@ -0,0 +1,14 @@
+package hello.entity;
+
+public class Ingredients {
+
+    private String name;
+
+    public Ingredients(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
